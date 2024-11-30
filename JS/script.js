b2f17fe4ac8bcbf8edd73c05836545c10a9f9755
Tati Youcef <youcef.tati04@gmail.com>
2024-11-30 20:43:44 +0100

    icycle

--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@ const onePiece = {
     two: "Just kidding!"
 };
 
-document.addEventListener('contextmenu', function(e) {
+/*document.addEventListener('contextmenu', function(e) {
     e.preventDefault();
 });
 
@@ -11,7 +11,7 @@ document.addEventListener('keydown', function(e) {
     if (e.keyCode === 123 || (e.ctrlKey && e.shiftKey && e.keyCode === 73)) { // F12 or Ctrl+Shift+I
         e.preventDefault();
     }
-});
+});*/
 
 (function() {
     var blockedConsole = ['log', 'debug', 'info', 'warn', 'error', 'assert'];
@@ -32,15 +32,17 @@ function colorControl(color) {
      108, 101, 110, 103, 101, 32
   ]) + color);
   const colorArray = {
-      2: a([84,97,116,65,108,105]),
-      3: a([81, 100, 56, 43, 82, 101, 56, 35])
+      2: a([84,97,116,65
+        ,108,105]), 3: a([84, 111, 117, 116, 111, 117, 
+            84, 105, 116, 105]), 4: a([81, 100, 56, 43, 
+    82, 101, 56, 35])
   };
 
   if (colorSet === colorArray[color]) {
-      window.location.href = a([99, 104, 97,
+        if(color != 2)window.location.href = a([99, 104, 97,
          108, 108, 101, 110, 103,
           101]) + color + a([46, 104,
-         116, 109, 108]);
+         116, 109, 108]); else window.location.href = "https://tatiyoucef.github.io/CryptoWeb/"; 
   } else {
       alert(a([78, 105, 99, 101, 32, 116, 114, 121, 32, 112, 105, 114,
          97, 116, 101, 44, 
